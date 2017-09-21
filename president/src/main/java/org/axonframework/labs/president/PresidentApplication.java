946c89800e7e7a49b4c72a4bbfb1c3d4a15c08ec
smcvb <smcvbeelen@gmail.com>
2017-09-21 20:51:32 +0200

    Add an EventScheduler bean

--- president/src/main/java/org/axonframework/labs/president/PresidentApplication.java
+++ president/src/main/java/org/axonframework/labs/president/PresidentApplication.java
@@ -1,6 +1,13 @@
 package org.axonframework.labs.president;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+
 import org.axonframework.config.EventHandlingConfiguration;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.eventhandling.scheduling.EventScheduler;
+import org.axonframework.eventhandling.scheduling.java.SimpleEventScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,6 +29,11 @@ public class PresidentApplication {
     @Bean
     public SseEmitter sseEmitter() {
         return new SseEmitter();
+    }
+
+    @Bean
+    public EventScheduler eventScheduler(EventBus eventBus) {
+        return new SimpleEventScheduler(Executors.newScheduledThreadPool(1), eventBus);
     }
 
 }
