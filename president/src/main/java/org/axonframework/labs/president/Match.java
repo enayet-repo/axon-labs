b2e186bc5675427e72cdbf53c7f1d06d62ddf72a
smcvb <smcvbeelen@gmail.com>
2017-09-21 15:53:53 +0200

    Adjust application to not reference a repository your self, but handle commands directly on the aggregate

--- president/src/main/java/org/axonframework/labs/president/Match.java
+++ president/src/main/java/org/axonframework/labs/president/Match.java
@@ -2,18 +2,14 @@ package org.axonframework.labs.president;
 
 import static org.axonframework.commandhandling.model.AggregateLifecycle.apply;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
+import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.commandhandling.model.AggregateIdentifier;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
-@Entity
 @Aggregate
 public class Match {
 
-    @Id
     @AggregateIdentifier
     private String aggregateIdentifier;
 
@@ -21,22 +17,27 @@ public class Match {
         // Required by Axon
     }
 
+    @CommandHandler
     public Match(CreateMatchCommand command) {
         apply(new MatchCreatedEvent(command.getAggregateIdentifier()));
     }
 
+    @CommandHandler
     public void handle(JoinMatchCommand cmd) {
         apply(new MatchJoinedEvent(cmd.getAggregateIdentifier()));
     }
 
+    @CommandHandler
     public void handle(StartMatchCommand cmd) {
         apply(new GameStartedEvent(cmd.getAggregateIdentifier()));
     }
 
+    @CommandHandler
     public void handle(PlayCardsCommand cmd) {
         apply(new CardsPlayedEvent(cmd.getAggregateIdentifier()));
     }
 
+    @CommandHandler
     public void handle(PassCommand cmd) {
         apply(new PlayerPassedEvent(cmd.getAggregateIdentifier()));
     }
--- president/src/main/java/org/axonframework/labs/president/PresidentApplication.java
+++ president/src/main/java/org/axonframework/labs/president/PresidentApplication.java
@@ -1,23 +1,13 @@
 package org.axonframework.labs.president;
 
-import org.axonframework.commandhandling.model.GenericJpaRepository;
-import org.axonframework.commandhandling.model.Repository;
-import org.axonframework.common.jpa.EntityManagerProvider;
-import org.axonframework.eventhandling.EventBus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PresidentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PresidentApplication.class, args);
-    }
-
-    @Bean
-    public Repository matchRepository(EntityManagerProvider entityManagerProvider, EventBus eventBus) {
-        return new GenericJpaRepository<>(entityManagerProvider, Match.class, eventBus);
     }
 
 }
