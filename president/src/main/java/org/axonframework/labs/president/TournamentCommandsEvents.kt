bcee0c69b503a82cf29ed1d43a0b69af47ac0413
smcvb <smcvbeelen@gmail.com>
2017-09-21 20:11:55 +0200

    Rename TournamentEvents file to TournamentCommandsEvents Add Round start event/command

---
+++ president/src/main/java/org/axonframework/labs/president/TournamentCommandsEvents.kt
@@ -0,0 +1,9 @@
+package org.axonframework.labs.president
+
+import org.axonframework.commandhandling.TargetAggregateIdentifier
+
+data class StartRoundCommand(@TargetAggregateIdentifier val tournamentId: String)
+
+data class TournamentCreatedEvent(val tournamentId: String)
+data class TournamentStartedEvent(val tournamentId: String)
+data class RoundStartedEvent(val tournamentId: String)
--- president/src/main/java/org/axonframework/labs/president/TournamentEvents.kt
+++
@@ -1,4 +0,0 @@
-package org.axonframework.labs.president
-
-data class TournamentCreatedEvent(val tournamentId: String)
-data class TournamentStartedEvent(val tournamentId: String)
