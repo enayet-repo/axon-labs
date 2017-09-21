9fdee8303af90a8fd361b38c6acdde15e158ac67
smcvb <smcvbeelen@gmail.com>
2017-09-21 15:50:38 +0200

    Adjust Match to be a non-event sourced aggregate

--- president/src/main/java/org/axonframework/labs/president/Match.java
+++ president/src/main/java/org/axonframework/labs/president/Match.java
@@ -2,12 +2,18 @@ package org.axonframework.labs.president;
 
 import static org.axonframework.commandhandling.model.AggregateLifecycle.apply;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
+@Entity
 @Aggregate
 public class Match {
 
+    @Id
     @AggregateIdentifier
     private String aggregateIdentifier;
 
@@ -33,6 +39,11 @@ public class Match {
 
     public void handle(PassCommand cmd) {
         apply(new PlayerPassedEvent(cmd.getAggregateIdentifier()));
+    }
+
+    @EventSourcingHandler
+    public void on(MatchCreatedEvent event) {
+        aggregateIdentifier = event.getAggregateIdentifier();
     }
 
 }
