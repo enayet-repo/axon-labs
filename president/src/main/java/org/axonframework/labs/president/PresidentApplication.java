6395c038a716857ee00d0d01028cc6f1fe4c0c23
smcvb <smcvbeelen@gmail.com>
2017-09-21 13:40:45 +0200

    Add project set up

---
+++ president/src/main/java/org/axonframework/labs/president/PresidentApplication.java
@@ -0,0 +1,12 @@
+package org.axonframework.labs.president;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresidentApplication.class, args);
+	}
+}
---
+++ president/src/test/java/org/axonframework/labs/president/PresidentApplicationTests.java
@@ -0,0 +1,16 @@
+package org.axonframework.labs.president;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PresidentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
