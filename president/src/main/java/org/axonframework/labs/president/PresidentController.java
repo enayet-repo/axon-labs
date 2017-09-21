b9857b34db66e2682aed5133056ed91953c2ed4a
smcvb <smcvbeelen@gmail.com>
2017-09-21 14:57:57 +0200

    Add commandGateway and get mappings for all commands

--- president/src/main/java/org/axonframework/labs/president/PresidentController.java
+++ president/src/main/java/org/axonframework/labs/president/PresidentController.java
@@ -1,14 +1,53 @@
 package org.axonframework.labs.president;
 
+import java.util.UUID;
+
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PresidentController {
 
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public PresidentController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
     @GetMapping
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    @GetMapping("/create-match")
+    public String createMatch() {
+        String aggregateIdentifier = UUID.randomUUID().toString();
+        commandGateway.send(new CreateMatchCommand(aggregateIdentifier));
+        return aggregateIdentifier;
+    }
+
+    @GetMapping("/join-match/{aggregateIdentifier}")
+    public void joinMatch(@PathVariable String aggregateIdentifier) {
+        commandGateway.send(new JoinMatchCommand(aggregateIdentifier));
+    }
+
+    @GetMapping("/start-match/{aggregateIdentifier}")
+    public void startMatch(@PathVariable String aggregateIdentifier) {
+        commandGateway.send(new StartMatchCommand(aggregateIdentifier));
+    }
+
+    @GetMapping("/play-cards/{aggregateIdentifier}")
+    public void playCards(@PathVariable String aggregateIdentifier) {
+        commandGateway.send(new PlayCardsCommand(aggregateIdentifier));
+    }
+
+    @GetMapping("/pass/{aggregateIdentifier}")
+    public void pass(@PathVariable String aggregateIdentifier) {
+        commandGateway.send(new PassCommand(aggregateIdentifier));
     }
 
 }
