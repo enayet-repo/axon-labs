85b901fa3c017d63abc4486b32380e85aad3053a
smcvb <smcvbeelen@gmail.com>
2017-09-21 16:12:43 +0200

    Let all event handlers default to TrackingEventProcessor

--- president/src/main/java/org/axonframework/labs/president/PresidentApplication.java
+++ president/src/main/java/org/axonframework/labs/president/PresidentApplication.java
@@ -1,5 +1,7 @@
 package org.axonframework.labs.president;
 
+import org.axonframework.config.EventHandlingConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,11 @@ public class PresidentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PresidentApplication.class, args);
+    }
+
+    @Autowired
+    public void setProcessingGroup(EventHandlingConfiguration eventHandlingConfiguration) {
+        eventHandlingConfiguration.usingTrackingProcessors();
     }
 
 }
