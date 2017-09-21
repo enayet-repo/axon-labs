6303a9f176c85cc007453f846388f7f0c0890090
smcvb <smcvbeelen@gmail.com>
2017-09-21 19:41:43 +0200

    Drop Tournament aggregate, commands, events and controller, as it's not needed

--- president/src/main/java/org/axonframework/labs/president/TournamentCommandsEvents.kt
+++
@@ -1,9 +0,0 @@
-package org.axonframework.labs.president
-
-import org.axonframework.commandhandling.TargetAggregateIdentifier
-
-data class CreateTournamentCommand(@TargetAggregateIdentifier val tournamentId: String)
-data class StartTournamentCommand(@TargetAggregateIdentifier val tournamentId: String)
-
-data class TournamentCreatedEvent(val tournamentId: String)
-data class TournamentStartedEvent(val tournamentId: String)
