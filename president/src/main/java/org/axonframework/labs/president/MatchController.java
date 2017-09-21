0ec1f208840b52c95cbd75aa6b9bd105bf1e3fd1
smcvb <smcvbeelen@gmail.com>
2017-09-21 18:30:17 +0200

    Rename controller to MatchController

--- president/src/main/java/org/axonframework/labs/president/MatchController.java
+++ president/src/main/java/org/axonframework/labs/president/MatchController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PresidentController {
+public class MatchController {
 
     @GetMapping
     public String helloWorld() {
