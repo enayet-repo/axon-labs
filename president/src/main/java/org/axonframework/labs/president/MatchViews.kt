634c6b008cacdddb264f34a4efc645e703277897
smcvb <smcvbeelen@gmail.com>
2017-09-21 16:55:28 +0200

    Add defaults to MatchView

--- president/src/main/java/org/axonframework/labs/president/MatchViews.kt
+++ president/src/main/java/org/axonframework/labs/president/MatchViews.kt
@@ -2,7 +2,7 @@ package org.axonframework.labs.president
 
 data class MatchView(
         val matchId: String,
-        val gameId: String,
+        val gameId: String? = null,
         val matchName: String,
-        val playerNames: MutableList<String>
+        val playerNames: MutableList<String> = mutableListOf()
 )
